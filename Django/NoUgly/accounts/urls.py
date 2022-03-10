bbb78e69bb71758134e26f4dab18f4d9ca832b99
paka_coding <wnstjd5408@naver.com>
2022-03-10 22:31:20 +0900

    account app에 url 수정

--- Django/NoUgly/accounts/urls.py
+++ Django/NoUgly/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
 
@@ -11,6 +11,6 @@ router.register(r'destination', DestinationViewSet)
 urlpatterns = [
     path('login/kakao/', kakao_login, name='kakao_login'),
     path('login/kakao/callback', kakao_callback, name='kakao_callback'),
-
+    path('', include(router.urls)),
 
 ]
